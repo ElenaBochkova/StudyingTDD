5544361eda3353191da9abd500d0d2a844994d2b
elenabochkova <planeado@gmail.com>
2020-03-17 21:01:01 +0300

    In the middle of Chapter 5

--- functional_tests.py
+++ functional_tests.py
@@ -43,20 +43,26 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1. Buy a cup of coffee' for row in rows),
-            "Новый элемент списка не появился в таблице")
-        self.fail('Закончить тест')
+        self.assertIn('1. Buy a cup of coffee', [row.text for row in rows])
+        
 
-#Текстовое поле по-прежнему приглашает добавить еще один элемент
-#Мы вводим "выпить кофе" (не зря же покупали!)
+        #Текстовое поле по-прежнему приглашает добавить еще один элемент
+        #Мы вводим "выпить кофе" (не зря же покупали!)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Drink the cup of coffee')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-#Страница снова обнавляется, и теперь показывает оба элемента списка
+        #Страница снова обнавляется, и теперь показывает оба элемента списка
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1. Buy a cup of coffee', [row.text for row in rows])
+        self.assertIn('1. Drink the cup of coffee', [row.text for row in rows])
 
 #Нам интересно, запомнит ли сайт наш список.
 #Здесь мы видим, что сайт сгенерировал для нас уникальный URL-адрес - об этом
 #выводится небольшой текст с объяснениями
-
+        self.fail('Закончить тест')
 #Мы посещаем этот URL-адрес - список по-прежнему там
 
 #На этом можно завершать тест
