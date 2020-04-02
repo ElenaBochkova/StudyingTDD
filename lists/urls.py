1fc15c788442f848fccca3c0f7f3adc0e71b19b5
elenabochkova <planeado@gmail.com>
2020-04-02 17:21:46 +0300

    в середине 7 главы

--- lists/urls.py
+++ lists/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
@@ -6,4 +7,7 @@ app_name = 'lists'
 urlpatterns = [
     #Домашняя страница
     path('', views.home_page, name='home'),
+    #path('/lists/one_in_the_world_list/', views.view_list, name = 'view_list'),
+    url(r'^lists/one_in_the_world_list/$', views.view_list,
+        name='view_list'),
     ]
--- lists/views.py
+++ lists/views.py
@@ -8,8 +8,13 @@ def home_page(request):
     '''Домашняя страница приложения Lists'''
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('lists/one_in_the_world_list')
 
     items = Item.objects.all()
     return render(request, 'lists/home.html', {'items': items})
+
+def view_list(request):
+    '''представление списка'''
+    items = Item.objects.all()
+    return render(request, 'lists/list.html', {'items': items})
     
