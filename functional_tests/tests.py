20d3ed3ec56258e0adae6db8d116578b74c197bf
elenabochkova <planeado@gmail.com>
2020-04-10 19:06:17 +0300

    Using Bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -6,7 +7,7 @@ import time
 import unittest
 from lists.models import Item
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """ Тест нового посетителя"""
 
     def clear_items(self):
