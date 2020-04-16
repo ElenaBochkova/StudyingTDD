30573f44ecd6ef64553ed57c8f6ef24b89c97a7b
elenabochkova <planeado@gmail.com>
2020-04-16 16:05:20 +0300

    changes

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
 from django.conf.urls import url
 from lists import views as list_views
 from lists import urls as list_urls
