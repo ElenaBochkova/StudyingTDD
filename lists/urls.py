188e38b1db1ba68b63f3248a59e8c002d6ca672d
elenabochkova <planeado@gmail.com>
2020-04-16 16:19:29 +0300

    changes

--- lists/urls.py
+++ lists/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+#from django.urls import path
 from django.conf.urls import url
 
 from . import views
@@ -6,7 +6,7 @@ from . import views
 app_name = 'lists'
 urlpatterns = [
     #Домашняя страница
-    path('', views.home_page, name='home'),
+    #path('', views.home_page, name='home'),
     #path('/lists/one_in_the_world_list/', views.view_list, name = 'view_list'),
     url(r'^new$', views.new_list, name = 'new_list'),
     url(r'^(\d+)/$', views.view_list,
